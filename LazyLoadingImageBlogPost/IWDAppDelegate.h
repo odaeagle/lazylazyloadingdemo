873c4516b3930ba9c0026b5e6cdc3a833596b708
Eagle Diao <eaglediao@Mobile-Team-iMac.local>
2013-03-08 13:51:13 -0500

    Initial Commit

---
+++ LazyLoadingImageBlogPost/IWDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  IWDAppDelegate.h
+//  LazyLoadingImageBlogPost
+//
+//  Created by Eagle Diao on 2013-03-08.
+//  Copyright (c) 2013 intelliware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IWDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LazyLoadingImageBlogPost/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LazyLoadingImageBlogPost
+//
+//  Created by Eagle Diao on 2013-03-08.
+//  Copyright (c) 2013 intelliware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "IWDAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([IWDAppDelegate class]));
+    }
+}
